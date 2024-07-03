646b06febe46481c5fa9f0b1eb728695ad509f0c
sllt <hello@sllt.me>
2024-07-03 09:44:53 +0800

    added example

--- _example/main.go
+++ _example/main.go
@@ -11,6 +11,7 @@ func main() {
 	server := socks5.NewServer(
 		socks5.WithLogger(socks5.NewLogger(log.New(os.Stdout, "socks5: ", log.LstdFlags))),
 	)
+	defer server.Stop()
 
 	// Create SOCKS5 proxy on localhost port 8000
 	if err := server.ListenAndServe("tcp", ":10800"); err != nil {
